0fb57c2d2920eac3300bc17fab040c3e61260980
Fabian Buch <fabian.buch@dm.de>
2017-09-08 14:14:59 +0200

    remove unused script (included in deploy_to_maven_central.sh now)

--- .travis/before-deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_8edb3a2488b9_key -iv $encrypted_8edb3a2488b9_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
-    gpg --fast-import .travis/codesigning.asc
-fi
